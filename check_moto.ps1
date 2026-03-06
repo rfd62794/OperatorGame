323b9b9f1587a6a795f7c14418f5ea9bb8b0bb37
ConsumrBuzzy <robert.d@consumrbuzz.com>
2026-03-06 06:24:30 -0500

    refactor: Use `$ADB` variable to specify and execute `adb` commands.

--- check_moto.ps1
+++ check_moto.ps1
@@ -1,4 +1,5 @@
 # OPERATOR: MOTO G 2025 HARDWARE HANDSHAKE
+$ADB = "C:\Users\cheat\AppData\Local\Android\Sdk\platform-tools\adb.exe"
 Write-Host "--- Scanning Moto G (Power/5G) for Linker/Memory Errors ---" -ForegroundColor Cyan
-adb logcat -c  # Clear buffer
-adb logcat *:E # Stream only Errors to terminal
+& $ADB logcat -c  # Clear buffer
+& $ADB logcat *:E # Stream only Errors to terminal
