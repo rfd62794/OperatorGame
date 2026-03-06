42aa89a2f23ac1ce258b9543241680b2143df987
ConsumrBuzzy <robert.d@consumrbuzz.com>
2026-03-06 08:05:54 -0500

    dfgh

--- src/lib.rs
+++ src/lib.rs
@@ -44,10 +44,3 @@ fn android_main(app: android_activity::AndroidApp) {
     ).expect("Failed to run on Android");
 }
 
-#[cfg(target_os = "android")]
-pub mod android_stubs {
-    // These core runtime symbols are still required by the Moto G loader 
-    // even when libc++_shared.so is bundled.
-    #[no_mangle] pub unsafe extern "C" fn __cxa_pure_virtual() { loop {} }
-    #[no_mangle] pub unsafe extern "C" fn __gxx_personality_v0() { loop {} }
-}
