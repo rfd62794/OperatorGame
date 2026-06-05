9c965f7ea2c8a77a553501bfdb68c23d125c5b14
rfd62794 <cheater2478@gmail.com>
2026-06-04 23:23:15 -0400

    chore: add wasm-bindgen dependency and remove trailing whitespace
    Add wasm-bindgen 0.2 to wasm32 target dependencies. Clean up trailing whitespace in android_main function and remove extra blank line at end of file.

--- src/lib.rs
+++ src/lib.rs
@@ -25,15 +25,15 @@ fn android_main(app: android_activity::AndroidApp) {
     use winit::platform::android::EventLoopBuilderExtAndroid;
 
     std::env::set_var("RUST_BACKTRACE", "full");
-    
+
     // ADR-042 Revision: Use the system-provided internal data path instead of hardcoding.
     let mut path = app.internal_data_path().unwrap_or_else(|| std::path::PathBuf::from("/data/local/tmp"));
     path.push("save.json");
-    
+
     let state = load(&path).unwrap_or_default();
 
     let mut options = eframe::NativeOptions::default();
-    
+
     // The 0.27 way to pass the app handle:
     options.event_loop_builder = Some(Box::new(move |builder| {
         builder.with_android_app(app);
@@ -45,4 +45,3 @@ fn android_main(app: android_activity::AndroidApp) {
         Box::new(|cc| Box::new(OperatorApp::new(cc, state, path))),
     ).expect("Failed to run on Android");
 }
-
